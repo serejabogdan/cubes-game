0d74f79335f7dc3d8d6da27cf49b6d4a95e0fc19
Hasst <sergey1112345@gmail.com>
2020-11-10 15:22:06 +0200

    added: main algorithm of cube formation

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,16 +1,58 @@
 import React from 'react';
 import './Main.css';
 
-const Main = () => {
-  return (
-    <div className="main">
-      <div className="main__playground"></div>
-      <div className="main__results">
-        <h2>Result</h2>
-        <h2>Table</h2>
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      start: false,
+      cubes: []
+    };
+
+    this.playground = React.createRef();
+  }
+
+  componentDidMount() {
+    this.setState((prevState) => ({...prevState, cubes: this.initCubes()}));
+  }
+
+  initCubes(amountCubes = 10) {
+    let cubes = [];
+    for (let i = 0; i < amountCubes; i++) {
+      cubes.push(this.getCube(i));
+    }
+    return cubes;
+  }
+
+  getPlaygroundSizes(cubeSize = 40) {
+    return {
+      width: this.playground.current.offsetWidth - cubeSize,
+      height: this.playground.current.offsetHeight - cubeSize
+    };
+  }
+
+  getCube(index) {
+    const {width, height} = this.getPlaygroundSizes();
+    const cubeStyles = {
+      left: Math.random() * width,
+      top: Math.random() * height
+    };
+    return <div key={index} className="cube" style={cubeStyles}></div>;
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <div className="main__playground" ref={this.playground} onClick={(e) => console.log(e.target.className)}>
+          {this.state.cubes}
+        </div>
+        <div className="main__results">
+          <h2>Result</h2>
+          <h2>Table</h2>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Main;
