7670f2e7fcd3a4b3e377d237901373b3f99dd226
Hasst <sergey1112345@gmail.com>
2020-11-11 19:35:35 +0200

    created: ResultTable component

---
+++ src/Main/ResultTable/ResultTable.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ResultTable.css';
+
+const ResultTable = () => {
+  return (
+    <div className="results">
+      <h2>Result</h2>
+      <h2>Table</h2>
+    </div>
+  );
+};
+
+export default ResultTable;
