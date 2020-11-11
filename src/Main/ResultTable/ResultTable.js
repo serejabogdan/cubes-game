fe34447d485732953b7861312df0e497f45b3da4
Hasst <sergey1112345@gmail.com>
2020-11-12 01:45:26 +0200

    added: sort result table

--- src/Main/ResultTable/ResultTable.js
+++ src/Main/ResultTable/ResultTable.js
@@ -5,6 +5,7 @@ import './ResultTable.css';
 const ResultTable = () => {
   const formingTableRows = () => {
     const players = getLocalStorage('players') || [];
+    players.sort((prev, next) => next.points - prev.points);
     return players.map((player, index) => (
       <tr key={`${player.nickname}${player.points}`}>
         <th scope="row">{index + 1}</th>
@@ -13,6 +14,7 @@ const ResultTable = () => {
       </tr>
     ));
   };
+
   return (
     <div className="results">
       <h2>results</h2>
