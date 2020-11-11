7043b4c0004a7e21a94066fbe9f979a2fda275c6
Hasst <sergey1112345@gmail.com>
2020-11-11 17:33:53 +0200

    added: Modal component to App component

--- src/App/App.js
+++ src/App/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Modal from '../Modal/Modal';
 
 function App() {
   return (
     <div className="App container">
+      <Modal />
       <Header />
       <Main />
     </div>
